d16e3f86179893a7937ee45266ac1b97f283138a
Sean Tseng <latte0205@gmail.com>
2022-07-04 11:39:43 +0800

    create other page with plus and divide

--- main.go
+++ main.go
@@ -1,22 +1,70 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := AddValues(2, 2)
+
+	fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+// addValues adds to intergers and return the sum
+func AddValues(x, y int) int {
+	return x + y
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	// x := 100.0
+	// y := 10.0
+	f, err := divideValues(100.0, 0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+
+		return
+	}
+	// divide := divideValues(2, 2)
+
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprint(w, "Hello, world!")
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	n, err := fmt.Fprint(w, "Hello, world!")
 
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
 
-	})
+	// })
 
-	_ = http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 
 }
